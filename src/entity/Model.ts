8055892164eafe1fd60c1a1f842c0d8354ad9315
devFendrix <dev.fendrix@gmail.com>
2023-03-03 16:55:51 +0100

    add two columns

--- src/entity/Model.ts
+++ src/entity/Model.ts
@@ -4,13 +4,17 @@ import mongoose from 'mongoose'
 export const CreateModelSchema = joi.object({
     name: joi.string().required(),
     artistFullname: joi.string().required(),
-    valid: joi.boolean().required()
+    valid: joi.boolean().required(),
+    approval: joi.number().required(),
+    disapproval: joi.number().required()
 }).required()
 
 export const UpdateModelSchema = joi.object({
     name: joi.string().required(),
     artistFullname: joi.string().required(),
-    valid: joi.boolean().required()
+    valid: joi.boolean().required(),
+    approval: joi.number().required(),
+    disapproval: joi.number().required()
 }).required()
 
 
@@ -18,7 +22,9 @@ const modelSchema = new mongoose.Schema({
     id: Number,
     name: String,
     artistFullname: String,
-    valid: Boolean
+    valid: Boolean,
+    approval: Number,
+    disapproval: Number
 })
 
 export const Model = mongoose.model('Model', modelSchema)
